f2a2ecdd13f1d07ffcb0c01ed7aa5d54dde8dc47
Mark Faulkner <markfaulk350@gmail.com>
2020-05-11 16:05:44 -0700

    small change to comments reguarding jwt auth middleware

--- @types/express/index.d.ts
+++ @types/express/index.d.ts
@@ -1,5 +1,5 @@
 // https://dev.to/kwabenberko/extend-express-s-request-object-with-typescript-declaration-merging-1nn5
-// This modifies the express Request interface so we can attach userData to the request in the JWT middleware
+// This modifies the express Request interface so we can attach currentUser to the request in the JWT middleware
 
 import { CurrentUser } from '../../src/interface/CurrentUser'
 
