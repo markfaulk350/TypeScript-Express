5d9cf80d13f7b1f28bd2ee8682eb2017c06e8be8
Mark Faulkner <markfaulk350@gmail.com>
2020-05-11 12:22:16 -0700

    added morgan logger and jsonwebtoken package

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 // 3rd Party Imports
 import express, { Application } from 'express'
+import morgan from 'morgan'
 import bodyParser from 'body-parser'
 
 // Personal Imports
@@ -13,6 +14,7 @@ const app: Application = express()
 app.set('port', 3000 || process.env.PORT)
 
 // 3rd Party Middleware
+app.use(morgan('dev'))
 app.use(bodyParser.urlencoded({ extended: true }))
 
 // Routes
